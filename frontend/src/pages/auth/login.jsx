16a63f0dd7d73c4cd874c52e1d08639441d23031
feline-fine <farwa.tariq.ahmed@gmail.com>
2025-04-11 16:28:47 +0500

    first commit with signup

---
+++ frontend/src/pages/auth/login.jsx
@@ -0,0 +1,8 @@
+export default function Login () {
+    return (
+        <div className="max-w-full flex items-center justify-center bg-amber-600">
+            hello, this is login
+            {/* <img/> */}
+        </div>
+    );
+}
